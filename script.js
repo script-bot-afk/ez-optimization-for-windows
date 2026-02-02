7256ce2d8fbcf8361486325c59d790ffb5abb705
NVT <nicowv123@gmail.com>
2026-02-01 21:21:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const output = document.getElementById("output");
 
 button.addEventListener("click", () => {
   output.textContent = "Have Fun!!";
+
+  window.open(
+    "https://github.com/script-bot-afk/ez-optimizations",
+    "_blank"
+  );
 });
